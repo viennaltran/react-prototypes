a9136b350a1eb99a2fe0e7f4439a79253396c2a4
viennaltran <lanvienhtran@gmail.com>
2018-11-07 12:56:48 -0800

    completed

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,31 @@
+import React from 'react';
+import Table from './table';
 
+export default () => {
+
+    const students = [
+        {
+            name:'Anna Tran',
+            course: 'Public Health',
+            grade: 85
+        },
+        {
+            name: 'Lana Tran',
+            course: 'Childhood Education',
+            grade:99
+        },
+        {
+            name:'Abbie La',
+            course:'Pharmaceutical Science',
+            grade: 70
+        }
+    ];
+    
+    return (
+    <div className="container">
+        <h1>Student Grade Table</h1>
+        <Table data={students}/>
+    </div>
+    )
+}
 
