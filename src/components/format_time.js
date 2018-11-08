790055302fef97ffd1f72f2484031ccf2857d97a
viennaltran <lanvienhtran@gmail.com>
2018-11-07 17:14:58 -0800

    finished

---
+++ src/components/format_time.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class FormatTime extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            hour: 0,
+            min: 0,
+            sec: 0,
+            ms: 0
+        }
+
+        this.timerStyle = {
+            display: 'inline-block',
+            position: 'relative',
+            left: '50%',
+            transform: 'translate(-50%)',
+            width: '295px'
+        }
+
+    }
+
+    componentWillReceiveProps(nextProps){
+        const { elapsed } = nextProps;
+
+        const sec = elapsed / 1000;
+        const min = sec / 60;
+
+        this.setState({
+            hour: Math.floor(min / 60),
+            min: Math.floor(min % 60),
+            sec: Math.floor(sec % 60),
+            ms: Math.floor(elapsed % 100)
+        });
+    }
+
+    render(){
+        const { hour, min, sec, ms } = this.state;
+
+        return (
+            <div style={this.timerStyle}>{hour}:{min}:{sec}.{ms}</div>
+        )
+    }
+}
+
+export default FormatTime;
