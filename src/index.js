cf3717866408d8343e49f80cdbb6f6251bf93621
viennaltran <lanvienhtran@gmail.com>
2018-11-19 12:08:39 -0800

    created a simple form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
+//import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+//create a react component
+const App = function () {
+    const buttonText ={text:'Click me'};
+    return (
+        <div>
+            <label className="label" htmlFor="name" >
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor:'blue', color:'white'}}>
+                {buttonText.text}
+            </button>
+            
+        </div>
+    )
+}
+
+//Take the react component and show it on the screen 
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
-    document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
